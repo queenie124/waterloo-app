1a91b5ec7485e46c7026bb7a2c3875570c471945
queenie124 <956585542@qq.com>
2023-09-23 09:32:43 -0400

    search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //Trial Comment
 
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
+import { Stores } from "./component/stores";
+ 
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -12,12 +13,28 @@ function App() {
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   }
-
+  
+  const [query,setQuery]= useState("fl");
+  console.log(Stores.filter(store=>store.name.toLowerCase().includes("fl"))); 
   return (
     <div className="App">
+      <div className="loginpage">
       {
         currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
       }
+      </div>
+    
+      <input 
+        type='text'
+        placeholder="search..."
+        className="search"
+        onChange={e => setQuery(e.target.value)}/> 
+      <ul className="list"> 
+        {Stores.filter(store=>store.name.toLowerCase().includes(query)
+        ).map((store)=> (
+          <li key={store.id}className="listItem">{store.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/component/stores.js
@@ -0,0 +1,17 @@
+export const Stores = [
+    {
+        "id":1,
+        "name":"flower store",
+        "location":"N2J4B9"
+    },
+    {
+        "id":2,
+        "name":"lanzhou noodle",
+        "location":"N2J3V5"
+    },
+    {
+        "id":3,
+        "name":"wstore",
+        "location":"N2J4V2"
+    } 
+]
