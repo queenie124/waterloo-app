3725ddd62a034008f1a88057f4b8eb214ede8510
Junzhang Luo <junzhangluo@Junzhangs-MacBook-Pro.local>
2024-04-27 23:58:53 -0400

    Update to spin wheel, three major issues. Grey gap, the sections on the wheel seem to be off, but transform in css looks correct, and the return value is not right, as well as the wheel can be reset during spinning

--- src/component/SpinWheel.js
+++ src/component/SpinWheel.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import "./SpinWheel.css";
+import React, { useState } from 'react';
+import './SpinWheel.css';
+import { Stores } from './stores';
+
 const SpinWheel = () => {
+    const [rotation, setRotation] = useState(0);
+    const [returnValue, setReturnValue] = useState(null);
+    const numStores = Stores.length;
+    const sectorAngle = 360 / numStores;
+    console.log(sectorAngle);
+
+    const handleSpin = () => {
+        const newValue = Math.ceil(Math.random() * 3600);
+        setRotation(rotation + newValue);
+
+        const wheelPosition = rotation % 360;
+        
+        const sectorIndex = Math.floor(wheelPosition / sectorAngle);
+        const store = Stores[sectorIndex];
+        setReturnValue(store.name);
+        console.log(returnValue);
+    };
+
     return (
-        <div class="container">
-            <div class="spinBtn">
-                <div class="wheel">
-                    <div class="number" style={{i:1, color: 'blue'}}>
-                        <span>100</span>
+        <div className="container">
+            <div className="spinBtn" onClick={handleSpin}>Spin</div>
+            <div className="wheel" style={{ transform: `rotate(${rotation}deg)`, '--num-sectors': Stores.length }}>
+                {Stores.map((store, index) => (
+                    <div key={store.id} className="sec" style={{ '--i': index + 1, '--clr': store.color }}>
+                        <span>{store.name}</span>
                     </div>
-                    <div class="number" style={{i:2, color: 'red'}}>
-                        <span>1</span>
-                    </div>
-                </div>
+                ))}
             </div>
+            {returnValue && <p>Selected Store: {returnValue}</p>}
         </div>
-    )
+    );
 };
+
 export default SpinWheel;
